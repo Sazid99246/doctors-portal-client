149c105311c9066a73544f4413a1bd0323ec97be
Sheikh MD Sazidul Islam <87424122+Sazidul008@users.noreply.github.com>
2022-05-11 17:20:05 +0600

    shown modal on button onclick

--- src/Pages/Appointment/Service/Service.js
+++ src/Pages/Appointment/Service/Service.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg shadow-xl">
             <div class="card-body">
                 <h2 class="card-title text-secondary">{name}</h2>
-                <p>{slots.length > 0 ? <span>{slots[0]}</span>:<span className='text-red-500 text-xl'>Try Another Date</span>}</p>
-                <p>{slots.length} {slots.length > 1 ? "spaces": "space"} available</p>
+                <p>{slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-500 text-xl'>Try Another Date</span>}</p>
+                <p>{slots.length} {slots.length > 1 ? "spaces" : "space"} available</p>
                 <div class="card-actions justify-center">
-                    <button disabled={slots.length === 0} class="btn btn-primary text-white uppercase">Book Appointment</button>
+                    <label
+                        for="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        class="btn btn-secondary text-white uppercase">Book Appointment</label>
                 </div>
             </div>
         </div>
